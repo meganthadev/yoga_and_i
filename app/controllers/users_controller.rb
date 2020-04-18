36246e2df10bbceb9891b979ff16a47283818279
Megan Horsfield <meganthadev@gmail.com>
2020-04-18 06:44:15 +0000

    add login routes for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,20 @@ class UsersController < ApplicationController
 
   post '/login' do
     @user = User.find_by(email: params[:email])
-    if @user.authenticate(password: params[:password])
-      @session = 
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      puts session
+      redirect "users/#{@user.id}"
     else
       "Invalid Password"
+   end
   end
 
   get '/signup' do
+  end
+
+  get '/users/:id' do
+    "This will be user show route"
   end
 
 end
