9e151e7763b72315d7e998065dcf6f67a8463027
Megan Horsfield <meganthadev@gmail.com>
2020-04-20 05:14:00 +0000

    Create set_current_user method in journal controller

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -17,13 +17,27 @@ class JournalEntriesController < ApplicationController
   end
 
   get '/journal_entries/:id' do
-    @journal_entry = JournalEntry.find(params[:id] )
+    set_journal_entry
     erb :'/journal_entries/show'
   end
 
   get '/journal_entries/:id/edit' do
-    @journal_entry = JournalEntry.find(params[:id] )
+    set_journal_entry
     erb :'/journal_entries/edit'
   end
+
+  patch '/journal_entries/:id' do
+    set_journal_entry
+    @journal_entry.update(content: params[:content] )
+    redirect "/journal_entries/#{@journal_entry.id}"
+  end
+
+ private
+
+ def set_journal_entry
+   @journal_entry = JournalEntry.find(params[:id] )
+ end
+
+
 
 end
