b5086b054f86abcdaf157958e81a3711b92e5cf6
Megan Horsfield <meganthadev@gmail.com>
2020-04-20 20:03:19 +0000

    Test commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   has_secure_password #<- Allows us to use AR method= authenticate, takes password as plain string and checks it against bcrypt's hashing algorithm to make sure its the correct pword
   has_many :journal_entries
 
+  
 
 end
