317c3d13969b59120ac62336f352252fd043e86a
Megan Horsfield <meganthadev@gmail.com>
2020-04-21 04:31:46 +0000

    Add requirement for login controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,13 @@ class UsersController < ApplicationController
 
   post '/login' do
     @user = User.find_by(email: params[:email])
-    if @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       puts session
       redirect "users/#{@user.id}"
     else
       "Invalid Password"
-      #redirect to login page
+      redirect '/login'
    end
   end
 
